249ed720d0982a81ad09c01fe34cbafe66cd66fd
Trina Das <your_email@example.com>
2024-08-29 14:57:12 -0400

    Initial commit with BrowserRouter setup and index.html changes

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Banner />
-      <Skills />
-      <Projects />
-      <Contact />
-      
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Banner />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
