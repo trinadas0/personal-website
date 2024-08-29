86f12da53c0ee291bc40b25919d31ad452a19d29
Trina Das <your_email@example.com>
2024-08-29 15:47:35 -0400

    init

--- src/App.js
+++ src/App.js
@@ -7,20 +7,17 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
-import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <NavBar />
-        <Banner />
-        <Skills />
-        <Projects />
-        <Contact />
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      
+    </div>
   );
 }
 
