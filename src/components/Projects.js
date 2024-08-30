4ae863da901087e706803a0c79af1d41c312dd77
Trina Das <your_email@example.com>
2024-08-30 15:54:11 -0400

    fixed import bug

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
