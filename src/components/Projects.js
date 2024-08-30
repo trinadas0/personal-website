5d07cd99b034e302a50e751f89b0b440102fa48c
Trina Das <your_email@example.com>
2024-08-29 23:48:02 -0400

    fixed fade animation

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
@@ -6,6 +7,8 @@ import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
 export const Projects = () => {
+
+  const [isVisibleOnce, setIsVisibleOnce] = useState(false);
 
   const projects = [
     {
@@ -20,58 +23,38 @@ export const Projects = () => {
       imgUrl: projImg1,
       link: "https://github.com/trinadas0/pytorch-image-classification",
     },
-    {
-      title: "PyTorch Image Classification",
-      description: "Building a simple CNN using PyTorch.",
-      imgUrl: projImg1,
-      link: "https://github.com/trinadas0/pytorch-image-classification",
-    },
-    {
-      title: "PyTorch Image Classification",
-      description: "Building a simple CNN using PyTorch.",
-      imgUrl: projImg1,
-      link: "https://github.com/trinadas0/pytorch-image-classification",
-    },
-    {
-      title: "PyTorch Image Classification",
-      description: "Building a simple CNN using PyTorch.",
-      imgUrl: projImg1,
-      link: "https://github.com/trinadas0/pytorch-image-classification",
-    },
-    {
-      title: "PyTorch Image Classification",
-      description: "Building a simple CNN using PyTorch.",
-      imgUrl: projImg1,
-      link: "https://github.com/trinadas0/pytorch-image-classification",
-    },
+    // Add more projects here...
   ];
 
   return (
-<section className="project" id="projects">
+    <section className="project" id="projects">
       <Container>
         <Row>
           <Col size={12}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                  {/* New div for the rounded background */}
-                  <div className="projects-bg">
-                    <h2>Projects</h2>
-                    <p>Explore my diverse range of projects, showcasing skills in software development, UX/UI design, and data science.</p>
-                    <Row>
-                      {
-                        projects.map((project, index) => {
+            <TrackVisibility partialVisibility>
+              {({ isVisible }) => {
+                if (isVisible && !isVisibleOnce) {
+                  setIsVisibleOnce(true);
+                }
+                return (
+                  <div className={isVisibleOnce ? "custom-fade-in" : "hidden"}>
+                    <div className="projects-bg">
+                      <h2>Projects</h2>
+                      <p>Explore my diverse range of projects, showcasing skills in software development, UX/UI design, and data science.</p>
+                      <Row>
+                        {projects.map((project, index) => {
                           return (
                             <ProjectCard
                               key={index}
                               {...project}
                             />
                           )
-                        })
-                      }
-                    </Row>
+                        })}
+                      </Row>
+                    </div>
                   </div>
-                </div>}
+                );
+              }}
             </TrackVisibility>
           </Col>
         </Row>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import TrackVisibility from 'react-on-screen';
 
 export const Skills = () => {
+
+  const [isVisibleOnce, setIsVisibleOnce] = useState(false);
+
   const responsive = {
     superLargeDesktop: {
       breakpoint: { max: 4000, min: 3000 },
@@ -25,32 +30,43 @@ export const Skills = () => {
   };
 
   return (
-    <section className="skill py-5" id="skills"> {/* Add py-5 for consistent padding */}
+    <section className="skill py-5" id="skills">
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <div className="skill-bx wow zoomIn p-5">
-              <h2 className="mb-4">Skills</h2>
-              <p className="mb-5">Proficient in Python, Java, C, web development with Bootstrap and React.js, and machine learning with PyTorch.</p>
-              <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                <div className="item">
-                  <img src={meter1} alt="Image" />
-                  <h5>Software Development</h5>
-                </div>
-                <div className="item">
-                  <img src={meter2} alt="Image" />
-                  <h5>Machine Learning</h5>
-                </div>
-                <div className="item">
-                  <img src={meter3} alt="Image" />
-                  <h5>Data Science</h5>
-                </div>
-                <div className="item">
-                  <img src={meter1} alt="Image" />
-                  <h5>UX/UI Design</h5>
-                </div>
-              </Carousel>
-            </div>
+            <TrackVisibility partialVisibility>
+              {({ isVisible }) => {
+                if (isVisible && !isVisibleOnce) {
+                  setIsVisibleOnce(true);
+                }
+                return (
+                  <div className={isVisibleOnce ? "custom-fade-in" : "hidden"}>
+                    <div className="skill-bx wow zoomIn p-5">
+                      <h2 className="mb-4">Skills</h2>
+                      <p className="mb-5">Proficient in Python, Java, C, web development with Bootstrap and React.js, and machine learning with PyTorch.</p>
+                      <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                        <div className="item">
+                          <img src={meter1} alt="Image" />
+                          <h5>Software Development</h5>
+                        </div>
+                        <div className="item">
+                          <img src={meter2} alt="Image" />
+                          <h5>Machine Learning</h5>
+                        </div>
+                        <div className="item">
+                          <img src={meter3} alt="Image" />
+                          <h5>Data Science</h5>
+                        </div>
+                        <div className="item">
+                          <img src={meter1} alt="Image" />
+                          <h5>UX/UI Design</h5>
+                        </div>
+                      </Carousel>
+                    </div>
+                  </div>
+                );
+              }}
+            </TrackVisibility>
           </div>
         </div>
       </div>
