2a4ae62abdd2cf80535ee81de61fd5e9699b807c
Trina Das <your_email@example.com>
2024-08-29 23:19:19 -0400

    removed project tabs

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,23 +1,43 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
 export const Projects = () => {
 
-  const codingProjects = [
+  const projects = [
     {
       title: "Book Tracker with PHP and SQL",
       description: "A simple Book Tracker application built with PHP and MySQL.",
       imgUrl: projImg2,
       link: "https://booktracker.das24.myweb.cs.uwindsor.ca/",
     },
-  ];
-
-  const AIProjects = [
+    {
+      title: "PyTorch Image Classification",
+      description: "Building a simple CNN using PyTorch.",
+      imgUrl: projImg1,
+      link: "https://github.com/trinadas0/pytorch-image-classification",
+    },
+    {
+      title: "PyTorch Image Classification",
+      description: "Building a simple CNN using PyTorch.",
+      imgUrl: projImg1,
+      link: "https://github.com/trinadas0/pytorch-image-classification",
+    },
+    {
+      title: "PyTorch Image Classification",
+      description: "Building a simple CNN using PyTorch.",
+      imgUrl: projImg1,
+      link: "https://github.com/trinadas0/pytorch-image-classification",
+    },
+    {
+      title: "PyTorch Image Classification",
+      description: "Building a simple CNN using PyTorch.",
+      imgUrl: projImg1,
+      link: "https://github.com/trinadas0/pytorch-image-classification",
+    },
     {
       title: "PyTorch Image Classification",
       description: "Building a simple CNN using PyTorch.",
@@ -27,62 +47,31 @@ export const Projects = () => {
   ];
 
   return (
-    <section className="project" id="projects">
+<section className="project" id="projects">
       <Container>
         <Row>
           <Col size={12}>
             <TrackVisibility>
               {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Explore my diverse range of projects, showcasing skills in software development, UX/UI design, and data science.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Coding</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">UX/UI Design</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Data and AI</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          codingProjects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                              />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="second">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <Row>
-                        {
-                          AIProjects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                              />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  {/* New div for the rounded background */}
+                  <div className="projects-bg">
+                    <h2>Projects</h2>
+                    <p>Explore my diverse range of projects, showcasing skills in software development, UX/UI design, and data science.</p>
+                    <Row>
+                      {
+                        projects.map((project, index) => {
+                          return (
+                            <ProjectCard
+                              key={index}
+                              {...project}
+                            />
+                          )
+                        })
+                      }
+                    </Row>
+                  </div>
+                </div>}
             </TrackVisibility>
           </Col>
         </Row>
